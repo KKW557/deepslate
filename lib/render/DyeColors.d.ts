3c91e5d5f01df2d570ce6468b8707dd51c8ace4b
KKW557 <kevin557557@gmail.com>
2025-03-19 01:09:32 +0800

    build

---
+++ lib/render/DyeColors.d.ts
@@ -0,0 +1,7 @@
+import { Color } from '../util/index.js';
+export declare const DyeColors: {
+    [key: string]: (props: {
+        [key: string]: string;
+    }) => Color;
+};
+//# sourceMappingURL=DyeColors.d.ts.map
